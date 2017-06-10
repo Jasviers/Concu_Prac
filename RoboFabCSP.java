75777153d3f5ddfe2f4ea2f19cd8bd1318e46f26
miquimus1 <juang97@hotmail.com>
2017-06-10 13:42:16 +0200

    Create RoboFabCSP.java

--- RoboFabCSP.java
+++ RoboFabCSP.java
@@ -1,3 +1,5 @@
+import java.awt.Robot;
+
 import org.jcsp.lang.Alternative;
 import org.jcsp.lang.AltingChannelInput;
 import org.jcsp.lang.Any2OneChannel;
@@ -6,25 +8,16 @@ import org.jcsp.lang.Channel;
 import org.jcsp.lang.ChannelOutput;
 import org.jcsp.lang.Guard;
 import org.jcsp.lang.One2OneChannel;
-
+    
 
 class PetNotificar {
-	private int robotId;
-	private int peso;
-
-	public PetNotificar(int robotId, int peso) {
-		this.robotId = robotId;
-		this.peso = peso;
-	}
-
-	public int getRobotId() {
-		return robotId;
-	}
-
-	public int getPeso() {
-		return peso;
-	}
-
+    int robotId;
+    int peso;
+    
+    public PetNotificar(int robotId, int peso) {
+	this.robotId = robotId;
+	this.peso = peso;
+    }
 }
 
 // RoboFabCSP: Solución con replicación de canales
@@ -33,108 +26,105 @@ class PetNotificar {
 public class RoboFabCSP implements RoboFab, CSProcess {
 
 
-	// Un canal para notificarPeso
-	Any2OneChannel chNotificar;
-	// NUM_ROBOTS canales para permisoSoltar
-	Any2OneChannel chSoltar[];
-	// Un canal para solicitarAvance
-	Any2OneChannel chAvanzar;
-	// Un canal para contenedorNuevo
-	Any2OneChannel chNuevo;
+    // Un canal para notificarPeso
+    Any2OneChannel chNotificar;
+    // NUM_ROBOTS canales para permisoSoltar
+    Any2OneChannel chSoltar[];
+    // Un canal para solicitarAvance
+    Any2OneChannel chAvanzar;
+    // Un canal para contenedorNuevo
+    Any2OneChannel chNuevo;
+    
+    public RoboFabCSP() {
 
-	public RoboFabCSP() {
+	// Creamos los canales
+    	chNotificar = Channel.any2one();
+	chSoltar = new Any2OneChannel[Robots.NUM_ROBOTS];
+	for (int i=0; i<Robots.NUM_ROBOTS; i++) chSoltar[i] = Channel.any2one();
+	chAvanzar = Channel.any2one();
+	chNuevo = Channel.any2one();
+    }
 
-		// Creamos los canales
-		chNotificar = Channel.any2one();
-		chSoltar = new Any2OneChannel[Robots.NUM_ROBOTS];
-		for (int i=0; i<Robots.NUM_ROBOTS; i++) chSoltar[i] = Channel.any2one();
-		chAvanzar = Channel.any2one();
-		chNuevo = Channel.any2one();
-	}
+    public void permisoSoltar(int robotId) {
+	chSoltar[robotId].out().write(null);
+    }
 
-	public void permisoSoltar(int robotId) {
-		chSoltar[robotId].out().write(null);
-	}
+    public void notificarPeso(int robotId, int peso) {
+	PetNotificar pet = new PetNotificar(robotId,peso);
+	chNotificar.out().write(pet);
+    }
 
-	public void notificarPeso(int robotId, int peso) {
-		PetNotificar pet = new PetNotificar(robotId,peso);
-		chNotificar.out().write(pet);
-	}
+    public void solicitarAvance() {
+	chAvanzar.out().write(null);
+    }
 
-	public void solicitarAvance() {
-		chAvanzar.out().write(null);
-	}
+    public void contenedorNuevo() {
+	chNuevo.out().write(null);
+    }
 
-	public void contenedorNuevo() {
-		chNuevo.out().write(null);
-	}
+    public void run() {
+	// declaramos estado del recurso: peso, pendientes...
+	int peso;//peso que tiene actualmente la caja
+	int[] pends;//Array de pesos pendientes que quedan por poner en la caja
 
-	public void run() {
-		// declaramos estado del recurso: peso, pendientes...
-		int peso;
-		int[] pends;
-
-		// Inicializamos el estado del recurso
-		peso = 0;
-		pends = new int[Robots.NUM_ROBOTS];
-		for(int i = 0; i<pends.length; i++){
-			pends[i]=0;
+	// Inicializamos el estado del recurso
+	peso = 0;
+	pends = new int[Robots.NUM_ROBOTS];
+	for(int i = 0; i<pends.length; i++){
+		pends[i]=0;
 		}
 
-		// Estructuras para recepción alternativa condicional
-		final AltingChannelInput[] guards = new AltingChannelInput[Robots.NUM_ROBOTS+3];
-		// reservamos NUM_ROBOTS entradas para permisoSoltar y una entrada cada una de
-		// notificarPeso, solicitarAvance y contenedorNuevo
+	// Estructuras para recepción alternativa condicional
+	final AltingChannelInput[] guards = new AltingChannelInput[Robots.NUM_ROBOTS+3];
+	// reservamos NUM_ROBOTS entradas para permisoSoltar y una entrada cada una de
+	// notificarPeso, solicitarAvance y contenedorNuevo
+	for(int i =3; i<Robots.NUM_ROBOTS;i++){
+		guards[i] = chSoltar[i].in();
+	}
+	final int NOTIFICAR = 0;
+	final int AVANZAR   = 1;
+	final int NUEVO     = 2;
+	// 
+	guards[NOTIFICAR] = chNotificar.in();
+	guards[AVANZAR]   = chAvanzar.in();
+	guards[NUEVO]     = chNuevo.in();
+
+	// array de booleanos para sincronización por condición
+	boolean enabled[] = new boolean[Robots.NUM_ROBOTS+3];
+	// inicializamos las condiciones de activación de los canales
+	
+	enabled[NOTIFICAR]= true;
+	enabled[NUEVO]= true;
+	final Alternative services = new Alternative(guards);
+
+	while (true) {
+	    // refrescamos el vector enabled:
+		boolean aux = true; //Creamos un booleano 
 		for(int i =0; i<Robots.NUM_ROBOTS;i++){
-			guards[i]=chSoltar[i].in();
+			enabled[i]= peso + pends[i] <= Cinta.MAX_P_CONTENEDOR;// Comprobamos el CPRE y ponemos a true o false dependiendo de si se cumple o no 
 		}
-		final int NOTIFICAR = Robots.NUM_ROBOTS;
-		final int AVANZAR   = Robots.NUM_ROBOTS+1;
-		final int NUEVO     = Robots.NUM_ROBOTS+2;
-		// 
-		guards[NOTIFICAR] = chNotificar.in();
-		guards[AVANZAR]   = chAvanzar.in();
-		guards[NUEVO]     = chNuevo.in();
+		
+		for(int i =0; i<Robots.NUM_ROBOTS && aux;i++){
+		if(peso+pends[i] <= Cinta.MAX_P_CONTENEDOR){ aux = false; }
+		}
 
-		// array de booleanos para sincronización por condición
-		boolean enabled[] = new boolean[Robots.NUM_ROBOTS+3];
-		// inicializamos las condiciones de activación de los canales
-		boolean aux = true;
-		for(int i =0; i<Robots.NUM_ROBOTS;i++){
-			enabled[i]=peso+pends[i] <= Cinta.MAX_P_CONTENEDOR;
-			aux = aux && !enabled[i];
-		}
-		enabled[NOTIFICAR]= true;
 		enabled[AVANZAR]= aux;
-		enabled[NUEVO]= true;
-		final Alternative services = new Alternative(guards);
-
-		while (true) {
-			// refrescamos el vector enabled:
-			aux = true;
-			for(int i =0; i<Robots.NUM_ROBOTS;i++){
-				enabled[i]=peso+pends[i] <= Cinta.MAX_P_CONTENEDOR;
-				aux = aux && !enabled[i];
-			}
-			enabled[NOTIFICAR]= true;
-			enabled[AVANZAR]= aux;
-			enabled[NUEVO]= true;
-
-			// la SELECT:
-			int i = services.fairSelect(enabled);
-			if (i == NOTIFICAR) {
-				ChannelOutput chResp = (ChannelOutput) chNotificar.in().read();
-				//pends[]= ;
-			} else if (i == AVANZAR) {
-				ChannelOutput chResp = (ChannelOutput) chAvanzar.in().read();
-			} else if (i == NUEVO) {
-				ChannelOutput chResp = (ChannelOutput) chNuevo.in().read();
-				peso = 0;
-			} else{ // permisoSoltar
-				ChannelOutput chResp = (ChannelOutput) chSoltar[i].in().read();
-				peso = pends[i];
-				pends[i]=0;
-			} 
-		}
-	}	
+		PetNotificar peticion= null;
+	    // la SELECT:
+	    int i = services.fairSelect(enabled);
+	    if (i == NOTIFICAR) {
+	    peticion = (PetNotificar)chNotificar.in().read();
+		pends[peticion.robotId] = peticion.peso;
+	    } else if (i == AVANZAR) {
+		chAvanzar.in().read();
+	    } else if (i == NUEVO) {
+		chNuevo.in().read();
+		peso=0;
+	    } else{ // permisoSoltar
+		chSoltar[i].in().read();
+		peso= peso + pends[i];
+		pends[i]=0;
+	    } 
+	}
+    }	
 }
